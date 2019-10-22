b8c9f96e59f07d493c6946f55b2e662a4522fdc1
Tyler <tylergross28@gmail.com>
2019-10-21 20:21:39 -0500

    just check if an array is sorted

--- src/main.c
+++ src/main.c
@@ -2,7 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+bool isArraySorted(int arr[], size_t lengthArr) {
+  for (size_t i = 1; i < lengthArr; i++) {
+    int currentItem = arr[i];
+    int lastItem = arr[i - 1];
+    if (currentItem < lastItem) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main() {
-  printf("Hello world!\n");
+  int arr[] = {2, 2, 58, 101, 4203, 5000};
+  size_t lengthArr = sizeof(arr) / sizeof(arr[0]);
+  printf("%s\n", isArraySorted(arr, lengthArr) ? "true" : "false");
+
   return EXIT_SUCCESS;
 }
